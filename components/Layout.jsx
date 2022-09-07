a842c7a679e8d667b57bbd6f99b81d7c0f788dd0
JB-Dev <jibaoke@gmail.com>
2022-09-07 12:25:08 +0200

    section card + banner footer + layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import  NavBar  from './NavBar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <meta charSet="utf-8" />
+        <title>
+          ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone
+        </title>
+        <link rel="shortcut icon" type="image/png" href="./icon.webp" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone, Ventes Promotion et exclusivité Iphone Apple Iwatch Earphones"
+        />
+      </Head>
+      <header>
+        <NavBar/>
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,22 @@
 import React from 'react'
-import Head from 'next/head';
+
 import {client} from '../lib/client'
 import {Product,FooterBanner,HeroBanner} from '../components'
 
 const Home = ({products,bannerData}) => {
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>
-          ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone
-        </title>
-        <link rel="shortcut icon" type="image/png" href="./icon.webp" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone, Ventes Promotion et exclusivité Iphone Apple Iwatch Earphones"
-        />
-      </Head>
-      <HeroBanner heroBanner={bannerData.length &&
-      bannerData[0]} />
-      {console.log(bannerData)}
-      <main>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
         <div className="products-heading">
           <h2>Nos Meilleures Ventes</h2>
           <p>Disponible en plusieurs Versions</p>
         </div>
         <div className="products-container">
-          {products?.map((product) => product.name)}
+          {products?.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
         </div>
-      </main>
-
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   );
 }
