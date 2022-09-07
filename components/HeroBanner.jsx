c0ad2f5c6d9472b05c284fa611d49d1697eb50c2
JB-Dev <jibaoke@gmail.com>
2022-09-07 10:27:25 +0200

    sanity and banner init

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="image-solo">Petit Text</p>
-        <h3>Text Moyen</h3>
-        <img src="" alt="Produits-Banniere" className="hero-banner-image" />
+        <p className="image-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="Produits-Banniere" className="hero-banner-image" />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">Text Boutton</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>
-              {' '}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium, necessitatibus! Voluptas, magni. Eius, commodi
-              molestiae!
-            </p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,50 @@
 import React from 'react'
+import Head from 'next/head';
+import {client} from '../lib/client'
 import {Product,FooterBanner,HeroBanner} from '../components'
 
-const Home = () => {
+const Home = ({products,bannerData}) => {
   return (
-    
-        <>
-          <HeroBanner/>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>
+          ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone
+        </title>
+        <link rel="shortcut icon" type="image/png" href="./icon.webp" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="ErphonX. Site E-commerce specialisé technologie haut de Gamme Iphone, Ventes Promotion et exclusivité Iphone Apple Iwatch Earphones"
+        />
+      </Head>
+      <HeroBanner heroBanner={bannerData.length &&
+      bannerData[0]} />
+      {console.log(bannerData)}
+      <main>
+        <div className="products-heading">
+          <h2>Nos Meilleures Ventes</h2>
+          <p>Disponible en plusieurs Versions</p>
+        </div>
+        <div className="products-container">
+          {products?.map((product) => product.name)}
+        </div>
+      </main>
 
-          <div className='products-heading'>
-            <h2>Nos Meilleures Ventes</h2>
-            <p>Disponible en plusieurs Versions</p>
-          </div>
-          <div className='products-container'>
-            {['Product 1', 'Product 2'].map((product)=> product)}
-          </div>
+      <FooterBanner />
+    </>
+  );
+}
 
-          <FooterBanner/>
-        </>       
-      
-    
-  )
+export const getServerSideProps = async ()=>{
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return{
+    props:{products,bannerData}
+  }
 }
 
 export default Home
